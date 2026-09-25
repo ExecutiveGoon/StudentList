b0a2fb53c98c1865eac39d80d1a50034f056374d
Lucas Brdar <brdarl@sheridancollege.ca>
2026-09-25 11:57:30 -0400

    whoops

--- src/main/java/student/StudentList.java
+++ src/main/java/student/StudentList.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
+// hi
 package student;
 
-import student.Student;
 import java.util.ArrayList;
 
 /**
